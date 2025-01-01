dc766a86020171b780030ec08b6c36d6c908e119
Anyelo Nuñez Obispo <anyelonuob2004@gmail.com>
2025-01-01 16:07:15 -0500

    Primer commit

---
+++ src/main/java/com/skydev/prueba_testing_API_REST/PruebaTestingApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.skydev.prueba_testing_API_REST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaTestingApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaTestingApiRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skydev/prueba_testing_API_REST/PruebaTestingApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skydev.prueba_testing_API_REST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaTestingApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
